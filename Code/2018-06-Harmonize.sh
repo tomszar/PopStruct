0f04fd49b82eb947c4a2efc372debffe66728752
tomszar <tomasgzarzar@gmail.com>
2018-06-15 09:47:54 -0400

    increased walltime

--- Code/2018-06-Harmonize.sh
+++ Code/2018-06-Harmonize.sh
@@ -45,7 +45,7 @@ do
 		echo "Writing job for $name"
 		echo "#!/bin/bash
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=10:00:00
+#PBS -l walltime=15:00:00
 #PBS -l pmem=8gb
 #PBS -A jlt22_b_g_sc_default #jlt22_b_g_sc_default or open
 #PBS -j oe
