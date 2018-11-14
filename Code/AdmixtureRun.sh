aed1ae9f34cc2e6f930d69594d1f6e66f1e873f9
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2018-11-14 10:52:34 -0500

    added --cv flag and improved comments

--- Code/AdmixtureRun.sh
+++ Code/AdmixtureRun.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-#This script will download Admixture software and run it using all plink files in the directory
+#This script will download Admixture software and run it using all plink files in the directory as input
 #It will calculate admixture using ks from 2 to 12
-#Remember to have this script as well as the genotype files you want to analyze in the same folder
-#To run it in background and output to a log file do the following:
-#chmod +x AdmixtureRun.sh
-#./AdmixtureRun.sh > AdmixtureRun.log 2>&1 &
+#Usage:
+#	- Place your plink binary files in the same folder as this script
+#	- To run it in background and output to a log file do the following in the console:
+#		$ chmod +x AdmixtureRun.sh
+#		$ ./AdmixtureRun.sh > AdmixtureRun.log 2>&1 &
 
 thisdir=$(pwd)
 wget http://www.genetics.ucla.edu/software/admixture/binaries/admixture_linux-1.3.0.tar.gz
 tar -xzf *.tar.gz
 echo $thisdir
 
-#We will request 4 cores per k per file
+#We will request 6 cores per k per file
 for file in *.bed
 do
 	for k in {2..12}
 	do
 		echo "#!/bin/bash
-#PBS -l nodes=1:ppn=4
+#PBS -l nodes=1:ppn=6
 #PBS -l walltime=48:00:00
 #PBS -l pmem=16gb
 #PBS -A open #jlt22_b_g_sc_default or open
@@ -26,7 +27,7 @@ do
 
 cd ${thisdir}
 
-admixture_linux-1.3.0/admixture -j4 ${file} ${k}" >> job_${file}_k${k}.pbs #Creating job to run admixture
+admixture_linux-1.3.0/admixture --cv -j4 ${file} ${k}" >> job_${file}_k${k}.pbs #Creating job to run admixture
 	qsub job_${file}_k${k}.pbs
 
 	done
